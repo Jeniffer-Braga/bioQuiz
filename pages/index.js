80f31bc8c7d9f2e9f20e62a4cfb704dbeba2a4b4
JenifferBraga <jeniffercunhabraga@gmail.com>
2021-02-20 15:42:01 -0300

    Conclusão da quinta aula da imersão react

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import styled from 'styled-components';
+import React from 'react';
+//import styled from 'styled-components';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router'; 
 import { useState } from 'react';
 
@@ -12,6 +14,7 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import Link from '../src/components/Link';
 
 export default function Home() {
   const router = useRouter();
@@ -26,7 +29,16 @@ export default function Home() {
       <QuizContainer>
         <QuizLogo />
 
-        <Widget>
+        <Widget 
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>BioQuiz</h1>
           </Widget.Header>
@@ -52,14 +64,54 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da galera</h1>
             <p>Dá uma olhada nesses quizes incríveis que o pessoal da Imersão React fez:</p>
+
+            <ul>
+            {db.external.map((linkExterno) => {
+              const [projectName, githubUser] = linkExterno
+                .replace(/\//g, '')
+                .replace('https:', '')
+                .replace('.vercel.app', '')
+                .split('.');
+
+              return (
+                <li key={linkExterno}>
+                  <Widget.Topic 
+                    as={Link}
+                    href={`/quiz/${projectName}___${githubUser}`}
+                  >
+                    {`${githubUser}/${projectName}`} 
+                  </Widget.Topic>
+                </li>
+              ); 
+            })}
+            </ul>
+
           </Widget.Content> 
         </Widget>
 
-        <Footer />
+        <Footer 
+          as={motion.footer}
+          transition={{ delay: 0.8, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
 
       </QuizContainer>
 
